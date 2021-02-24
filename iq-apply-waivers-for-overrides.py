74139174b701f74ad9c0dfc3642093c31931e231
Sola Otudeko <sola@miles.local>
2021-02-24 12:52:57 +0000

    8

--- iq-apply-waivers-for-overrides.py
+++ iq-apply-waivers-for-overrides.py
@@ -6,6 +6,7 @@ outputDir = './datafiles'
 overRidesCsvFile = '{}/{}'.format(outputDir, 'overrides.csv')
 appPolicyViolationsCsvFile = '{}/{}'.format(outputDir, 'apppolicyviolations.csv')
 summaryCsvFile = '{}/{}'.format(outputDir, 'applywaivers.csv')
+cmdFile = '{}/{}'.format(outputDir, "cmdfile.txt")
 
 overridesDb = fileIO.readOverridesFile(overRidesCsvFile)
 violationsDb = fileIO.readCsvFile(appPolicyViolationsCsvFile)
@@ -28,22 +29,34 @@ def getViolation(applicationId, componentHash, cve):
   return applicationPublicId, policyViolationId
 
 
+def writeCommand(applicationPublicId, policyViolationId):
+  iqUrl = "http://localhost:8070"
+  iqUser = "admin"
+  iqPwd = "admin123"
+  cmd = "curl -u " + iqUser + ":" + iqPwd + " -X POST -H \"Content-Type: application/json\" -d " + "'{\"comment\": \"adding waiver for status override\"}' " + iqUrl + "/api/v2/policyWaivers/application/" + applicationPublicId + "/" + policyViolationId
+  return cmd
+
 def main():
   summaryDB = []
 
-  for override in overridesDb:
-    applicationId = override[1]
-    overrideStatus = override[2]
-    packageUrl = override[4]
-    componentHash = override[5]
-    cve = override[6]
+  with open(cmdFile, 'w') as fd:
 
-    applicationPublicId, policyViolationId = getViolation(applicationId, componentHash, cve)
-    line = packageUrl + "," + cve + "," + overrideStatus + "," + applicationPublicId + "," + policyViolationId + "\n"
-    summaryDB.append(line)
+    for override in overridesDb:
+      applicationId = override[1]
+      overrideStatus = override[2]
+      packageUrl = override[4]
+      componentHash = override[5]
+      cve = override[6]
+
+      applicationPublicId, policyViolationId = getViolation(applicationId, componentHash, cve)
+      line = packageUrl + "," + cve + "," + overrideStatus + "," + applicationPublicId + "," + policyViolationId + "\n"
+      summaryDB.append(line)
+      cmd = writeCommand(applicationPublicId, policyViolationId)
+      fd.write(cmd)
 
   csvHeader = "PackageUrl,CVE,OverrideStatus,ApplicationPublicId,PolicyViolationId\n"
   fileIO.writeCSVFile(summaryCsvFile, csvHeader, summaryDB)
+  print(cmdFile)
 
 if __name__ == '__main__':
   main()
--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ iqUrl=$1
 iqUser=$2
 iqPwd=$3
 
+iqUrl=http://localhost:8070
+iqUser=admin
+iqPwd=admin123
+
 python3 iq-component-overrides.py ${iqUrl} ${iqUser} ${iqPwd}
 python3 iq-policyviolations-for-overrides.py ${iqUrl} ${iqUser} ${iqPwd}
 python3 iq-apply-waivers-for-overrides.py
